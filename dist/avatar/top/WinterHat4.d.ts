267a137eba7b72a6a7af9f09158e54c2c74156c2
Fang-Pen Lin <hello@fangpenlin.com>
2017-12-18 15:33:18 +0800

    Add winter hat 4

---
+++ dist/avatar/top/WinterHat4.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="react" />
+import * as React from 'react';
+export default class WinterHat4 extends React.Component {
+    static optionValue: string;
+    private filter1;
+    private mask1;
+    private mask2;
+    private mask3;
+    private mask4;
+    private path1;
+    private path2;
+    private path3;
+    private path4;
+    private path5;
+    render(): JSX.Element;
+}
