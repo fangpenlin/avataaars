7a1ee95bc4419a7efa68d088c5134f1985e376ee
David Francisco <dmfrancisc@gmail.com>
2018-05-24 08:53:31 +0100

    Replace lodash dependency with lodash.uniqueid

--- src/avatar/top/accessories/Round.tsx
+++ src/avatar/top/accessories/Round.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { uniqueId } from 'lodash'
+import * as uniqueId from 'lodash.uniqueid'
 
 export default class Round extends React.Component {
   static optionValue = 'Round'
