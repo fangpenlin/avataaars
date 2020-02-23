bf76a9ecea7b851facb2ce076dca3986a2926e2c
waffs <wpitallo@gmail.com>
2020-02-23 15:17:36 +0200

    Update Selector.tsx

--- src/options/Selector.tsx
+++ src/options/Selector.tsx
@@ -26,7 +26,7 @@ export default class Selector extends React.Component<Props> {
     return this.context.optionContext
   }
 
-  componentWillMount () {
+  componentDidMount () {
     const { option, defaultOption } = this.props
     const { optionContext } = this
     const defaultValue = (
