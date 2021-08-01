548ad8c70371f20f5f0f6e6b5d8e504d8bff01db
soulteary <soulteary@gmail.com>
2021-08-01 19:08:59 +0800

    Compatible to 16.14

--- src/index.tsx
+++ src/index.tsx
@@ -39,7 +39,7 @@ export default class AvatarComponent extends React.Component<Props> {
     return { optionContext: this.optionContext }
   }
 
-  componentWillMount () {
+  UNSAFE_componentWillMount () {
     this.updateOptionContext(this.props)
   }
 
